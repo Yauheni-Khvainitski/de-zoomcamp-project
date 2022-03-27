30de7a27f493d75c41c6847d44a39179b20eb6f1
Yauheni Khvainitski <aliakseikaravaichyk@192.168.10.9>
2022-03-27 21:04:13 +0300

    move dataset to DWH separate script

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,13 +40,13 @@ resource "google_storage_bucket" "data-lake-bucket" {
   force_destroy = true
 }
 
-# DWH
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = var.BQ_DATASET
-  project    = var.project
-  location   = var.region
-}
+# # DWH
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+# resource "google_bigquery_dataset" "dataset" {
+#   dataset_id = var.BQ_DATASET
+#   project    = var.project
+#   location   = var.region
+# }
 
 # airflow VM
 resource "google_compute_instance_from_machine_image" "vm" {
